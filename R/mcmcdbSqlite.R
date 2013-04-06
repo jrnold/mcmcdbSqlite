9406f3ffb8b51019d9ea1ffb0da7ed042289ecfe
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-04-06 09:41:21 -0400

    new SqlTable and SqlColumn classes

--- R/mcmcdbSqlite.R
+++ R/mcmcdbSqlite.R
@@ -15,59 +15,95 @@ NULL
 setClass("McmcdbSqlite", "Mcmcdb",
          representation = representation(connection = "SQLiteConnection"))
 
+
 setClass("SqlTable",
+         representation =
          representation(name = "character",
-                        columns = "data.frame",
+                        columns = "list",
                         constraints = "character"))
 
-table_chains <-
-  data.frame(name = c("chain_id"),
-             type = c("INTEGER"),
-             constraints = c("PRIMARY KEY"))
+SqlTable <- function(name, columns = list(), constraints = character()) {
+  new("SqlTable", name = name, columns = columns, constraints = constraints)
+}
 
-table_iter <-
-  data.frame(name = c("chain_id", "iter"),
-             type = c("INTEGER", "INTEGER"),
-             constraints = c("PRIMARY KEY", "PRIMARY KEY"))
 
-table_flatpars <-
-  data.frame(name = c("flatpar", "pararray", "offset"),
-             type = c("VARCHAR", "VARCHAR", "INTEGER"),
-             constraints = c("PRIMARY KEY", "", ""))
+setClass("SqlColumn",
+         representation =
+         representation(name = "character",
+                        type = "character",
+                        constraints = "character"))
 
-table_pararrays <-
-  data.frame(name = c("pararray", "ndim"),
-             type = c("VARCHAR", "INTEGER"),
-             constraints = c("PRIMARY KEY"))g
+SqlColumn <- function(name, type, constraints = character()) {
+  new("SqlColumn", name = name, type = type, constraints = constraints)
+}
 
-table_pardim <-
-  data.frame(name = c("pararray", "dim", "index"),
-             type = c("VARCHAR", "INTEGER", "INTEGER"),
-             constraints = c("PRIMARY KEY", "PRIMARY KEY", ""))
-
-table_flatpar_chains <-
-  data.frame(name = c("flatpar", "chain_id"),
-             type = c("VARCHAR", "INTEGER"),
-             constraints = c("PRIMARY KEY", ""))
-
-table_samples <-
-  data.frame(name = c("chain_id", "iter", "flatpar", "val"),
-             type = c("VARCHAR", "INTEGER", "INTEGER", "NUMERIC"),
-             constraints = c("PRIMARY KEY", "PRIMARY KEY", "PRIMARY KEY", ""))
-
-table_metadata <-
-  data.frame(name = c("name", "value"),
-             type = c("VARCHAR", "BLOB"))
-
-table_model_data <-
-  data.frame(name = c("name", "value"),
-             type = c("VARCHAR", "BLOB"),
-             contraints = c("PRIMARY KEY", ""))
-
-table_version <-
-  data.frame(name = c("versionid", "major", "minor", "patch"),
-             type = c("INTEGER", "INTEGER", "INTEGER", "INTEGER"),
-             contraints = c("PRIMARY KEY", "", "", ""))
+## tablelist <-
+##   list(
+##     chains =
+##     SqlTable(name = "chains",
+##              columns =
+##              list(SqlColumn("chain_id", "INTEGER",
+##                             "PRIMARY KEY")))
+##     ## iters = 
+##     ## SqlTable(name = "iters",
+##     ##          columns =
+##     ##          list(
+##     ##            SqlColumn(name = "chain_id",
+##     ##                      type = "INTEGER",
+##     ##                      constraints = "PRIMARY KEY"),
+               
+##     ##            type = c("INTEGER", "INTEGER")),
+##     ##          constraints = c("PRIMARY KEY", "PRIMARY KEY")),
+##     ## flatpars =
+##     ## SqlTable(name = "flatpars",
+##     ##          columns =
+##     ##          data.frame(name = c("flatpar", "pararray", "offset"),
+##     ##                     type = c("VARCHAR", "VARCHAR", "INTEGER")),
+##     ##          constraints = c("PRIMARY KEY", "", "")),
+##     ## pararrays =
+##     ## SqlTable(name = "pararrays",
+##     ##          columns =
+##     ##          data.frame(name = c("pararray", "ndim"),
+##     ##                     type = c("VARCHAR", "INTEGER")),
+##     ##          constraints = c("PRIMARY KEY")),
+##     ## pardim =
+##     ## SqlTable(name = "pardim",
+##     ##          columns = 
+##     ##          data.frame(name = c("pararray", "dim", "index"),
+##     ##                     type = c("VARCHAR", "INTEGER", "INTEGER")),
+##     ##          constraints = c("PRIMARY KEY", "PRIMARY KEY", "")),
+##     ## latpar_chains =
+##     ## SqlTable(name = "flatpar_chains",
+##     ##          columns =
+##     ##          data.frame(name = c("flatpar", "chain_id"),
+##     ##                     type = c("VARCHAR", "INTEGER")),
+##     ##          constraints = c("PRIMARY KEY", "")),
+##     ## samples =
+##     ## SqlTable(name = "samples",
+##     ##          columns = 
+##     ##          data.frame(name = c("chain_id", "iter", "flatpar", "val"),
+##     ##                     type = c("VARCHAR", "INTEGER", "INTEGER", "NUMERIC"))
+##     ##          constraints =
+##     ##          c("PRIMARY KEY", "PRIMARY KEY", "PRIMARY KEY", "")),
+##     ## metadata =
+##     ## SqlTable(name = "metadata",
+##     ##          columns =
+##     ##          data.frame(name = c("name", "value"),
+##     ##                     type = c("VARCHAR", "BLOB")),
+##     ##          constraints = c("PRIMARY KEY", "")),
+##     ## model_data =
+##     ## SqlTable(name = "model_data",
+##     ##          columns =
+##     ##          data.frame(name = c("name", "value"),
+##     ##                     type = c("VARCHAR", "BLOB"))
+##     ##          contraints = c("PRIMARY KEY", "")),
+##     ## version =
+##     ## SqlTable(name = "version",
+##     ##          columns =
+##     ##          data.frame(name = "version",
+##     ##                     type = "VARCHAR"),
+##     ##          constraints = c("PRIMARY KEY"))
+##     )
 
 #' Create \code{McmcdbSqlite} object
 #'
